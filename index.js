b7c555ff747292632abb0cc1f89d8a8a63ec4cf7
t-powers <127955989+t-powers@users.noreply.github.com>
2025-11-06 14:51:49 -0500

    Added html boilerplt and connection to js.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Happy developing ✨')
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const arithmetic =["+","-","x","÷","="];
+const controls = ["AC", ".", "+/-"];
+
+let firstNum = 0;
+let operator = null;
+let secondNum = null;

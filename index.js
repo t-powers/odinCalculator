4d9d8e8a18717836dd783bab3d20cf7fa1890725
t-powers <127955989+t-powers@users.noreply.github.com>
2025-11-06 15:13:43 -0500

    Created buttons

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const allButtons = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "+","-","x","÷","=","AC", ".", "+/-"];
 const arithmetic =["+","-","x","÷","="];
 const controls = ["AC", ".", "+/-"];
 
 let firstNum = 0;
 let operator = null;
 let secondNum = null;
+
+const display = document.getElementById("display")
+const buttons = document.getElementById("buttons");
+
+//attach numbers to buttons
+for (let i = 0; i < allButtons.length; i++) {
+    const button = document.createElement('button');
+    const value = allButtons[i];
+    button.innerText = value;
+
+
+}

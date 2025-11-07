45a933735e09b384d8f069e5de4605739eeb8166
t-powers <127955989+t-powers@users.noreply.github.com>
2025-11-07 09:44:19 -0500

    Npm updates

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-const allButtons = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "+","-","x","÷","=","AC", ".", "+/-"];
-const arithmetic =["+","-","x","÷","="];
+const allButtons = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "+",
+  "-",
+  "x",
+  "÷",
+  "=",
+  "AC",
+  ".",
+  "+/-",
+];
+const arithmetic = ["+", "-", "x", "÷", "="];
 const controls = ["AC", ".", "+/-"];
 
 let firstNum = 0;
 let operator = null;
 let secondNum = null;
 
-
 for (let i = 0; i < allButtons.length; i++) {
-    let button = document.createElement('button');
-    let value = allButtons[i];
-    button.innerText = value;
+  let button = document.createElement("button");
+  let value = allButtons[i];
+  button.innerText = value;
 
-    button.addEventListener('click', (e) => {
-        console.log("clicked");
-    })
+  button.addEventListener("click", (e) => {
+    console.log("clicked");
+  });
 
-
-
-
-
-
-
-
-
-    document.getElementById("buttons").appendChild(button);
-    const display = document.getElementById("display");
-
+  document.getElementById("buttons").appendChild(button);
+  const display = document.getElementById("display");
 }
